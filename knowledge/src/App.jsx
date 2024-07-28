728b4030d1b8221d46915a8790e9bd9946edeacb
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-07-28 12:31:01 +0600

    complete with image error...

--- knowledge/src/App.jsx
+++ knowledge/src/App.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react"
 import Blogs from "./components/Blogs/Blogs"
+import Bookmarks from "./components/Bookmarks/Bookmarks"
 import Header from "./components/Header/Header"
 
 
 function App() {
+  const [bookmarks,setbookmarks] = useState([]);
+
+  const handleAddToBookmarks = blog=>{
+    const newBookmarks = [...bookmarks,blog]
+    setbookmarks(newBookmarks)
+
+  }
+
 
   return (
     <>
     <Header />
-    <Blogs />
+    <main className="md:flex max-w-7xl mx-auto">
+      <Blogs 
+      handleAddToBookmarks=
+      {handleAddToBookmarks} 
+      />
+      <Bookmarks bookmarks={bookmarks}  />
+
+    </main>
 
 
     </>
--- knowledge/src/components/Header/Header.jsx
+++ knowledge/src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-function Header() {
-  return (
-    <div>
-        <h1 className='text-4xl'>Knowledge Cafe</h1>
+import profile from '../../assets/images/profile.png';
+const Header = () => {
+    return (
+        <header className='flex justify-between items-center p-4 border-b-2  max-w-7xl mx-auto'>
+            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <img src={profile} alt="" />
+        </header>
+    );
+};
 
-        
-    </div>
-  )
-}
-
-export default Header
+export default Header;
